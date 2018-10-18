4b3b1a1cad590225d1cbbdb31007b8351e089f19
Kevin <1025046794@qq.com>
2018-10-18 13:07:42 +0800

    路由分组

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use illuminate\support\facades\route;
 
 class HomeController extends Controller
 {
     public function index()
     {
+
+/*		$route = Route::current();
+
+		$name = Route::currentRouteName();
+
+		$action = Route::currentRouteAction();
+		dd($route, $name, $action);*/
         // 获取数据
         // 模版渲染
         return view('home/home');
--- routes/web.php
+++ routes/web.php
@@ -11,42 +11,46 @@
 |
 */
 
-// 首页
-Route::redirect('/', '/home', 301);//重定向
+//重定向首页
+Route::redirect('/', '/home', 301);
 // Route::get('/', function () {
 // 	return Redirect::to('home');
 // });
-Route::get('/home', '\App\Http\Controllers\Home\HomeController@index');
-/*学校模块*/
-Route::get('/schoolIntroduce', '\App\Http\Controllers\Home\SchoolController@index');// 学校介绍
-Route::get('/schoolNews', '\App\Http\Controllers\Home\SchoolController@schoolNews');// 学校新闻
-Route::get('/questionAndAnswer', '\App\Http\Controllers\Home\SchoolController@questionAndAnswer');// 相关问答
-Route::get('/environment', '\App\Http\Controllers\Home\SchoolController@environment');// 学校环境
-/*课程模块*/
-Route::get('/offerCourse', '\App\Http\Controllers\Home\CurriculumController@offerCourse');//开班信息
-Route::get('/courseDetail', '\App\Http\Controllers\Home\CurriculumController@courseDetail');//开班详情
-Route::get('/specialCourse', '\App\Http\Controllers\Home\CurriculumController@specialCourse');//特色课程
-Route::get('/curriculumDetail', '\App\Http\Controllers\Home\CurriculumController@curriculumDetail');//课程详情
-/*学员与就业*/
-Route::get('/Student', '\App\Http\Controllers\Home\StudentController@index');//学员展示
-Route::get('/StudentDetail', '\App\Http\Controllers\Home\StudentController@StudentDetail');//学员详情
-Route::get('/certificate', '\App\Http\Controllers\Home\StudentController@certificate');//证书查询
-/*瑜伽资讯*/
-Route::get('/knowledge', '\App\Http\Controllers\Home\InformationController@knowledge');//瑜伽知识
-Route::get('/knowledgeDetail', '\App\Http\Controllers\Home\InformationController@knowledgeDetail');//知识详情
-Route::get('/yogaNews', '\App\Http\Controllers\Home\InformationController@yogaNews');//瑜伽新闻
-Route::get('/newsDetail', '\App\Http\Controllers\Home\InformationController@newsDetail');// 新闻详情
-/*老师模块*/
-Route::get('/teacher', '\App\Http\Controllers\Home\TeacherController@teacher');// 老师列表
-Route::get('/teacherDetail', '\App\Http\Controllers\Home\TeacherController@teacherDetail');// 老师详情
-/*联系报名*/
-Route::get('/contact', '\App\Http\Controllers\Home\ContactController@contact');// 联系报名页面
+/* 前台模块 */
+Route::namespace('Home')->group(function () {
+	Route::get('/home', 'HomeController@index');
+	/*学校模块*/
+	Route::get('/schoolIntroduce', 'SchoolController@index');// 学校介绍
+	Route::get('/schoolNews', 'SchoolController@schoolNews');// 学校新闻
+	Route::get('/questionAndAnswer', 'SchoolController@questionAndAnswer');// 相关问答
+	Route::get('/environment', 'SchoolController@environment');// 学校环境
+	/*课程模块*/
+	Route::get('/offerCourse', 'CurriculumController@offerCourse');//开班信息
+	Route::get('/courseDetail', 'CurriculumController@courseDetail');//开班详情
+	Route::get('/specialCourse', 'CurriculumController@specialCourse');//特色课程
+	Route::get('/curriculumDetail', 'CurriculumController@curriculumDetail');//课程详情
+	/*学员与就业*/
+	Route::get('/Student', 'StudentController@index');//学员展示
+	Route::get('/StudentDetail', 'StudentController@StudentDetail');//学员详情
+	Route::get('/certificate', 'StudentController@certificate');//证书查询
+	/*瑜伽资讯*/
+	Route::get('/knowledge', 'InformationController@knowledge');//瑜伽知识
+	Route::get('/knowledgeDetail', 'InformationController@knowledgeDetail');//知识详情
+	Route::get('/yogaNews', 'InformationController@yogaNews');//瑜伽新闻
+	Route::get('/newsDetail', 'InformationController@newsDetail');// 新闻详情
+	/*老师模块*/
+	Route::get('/teacher', 'TeacherController@teacher');// 老师列表
+	Route::get('/teacherDetail', 'TeacherController@teacherDetail');// 老师详情
+	/*联系报名*/
+	Route::get('/contact', 'ContactController@contact');// 联系报名页面
+});
+
 
 
 /**
  * 后台路由
 */
-Route::group(['prefix' => '/admin'], function () {
+Route::prefix('admin')->group(function () {
 	/*后台首页*/
 	Route::get('/', '\App\Http\Controllers\Admin\AdminHomeController@index');
 	/* 学校模块 */
