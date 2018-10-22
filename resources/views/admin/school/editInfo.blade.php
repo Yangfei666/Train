bff2e7375bccde5c53aabe8ab1bf58313729a3eb
Kevin <1025046794@qq.com>
2018-10-22 18:12:14 +0800

    '后台提交学校信息'

---
+++ resources/views/admin/school/editInfo.blade.php
@@ -0,0 +1,11 @@
+@extends('adminLayout.base')
+
+@section('main')
+        <!-- Main Container Start -->
+        <div id="mws-container" class="clearfix">
+        
+        	<h2><center>欢迎来到澳洲亚太瑜伽联盟后台管理系统</center></h2>
+            
+        </div>
+        <!-- Main Container End -->
+@endsection
